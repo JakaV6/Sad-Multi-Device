31e3dc282043914aa8b2d3f94dbbbc97671ce3de
JakaV6 <108479524+JakaV6@users.noreply.github.com>
2022-07-08 20:06:55 +0700

    Update audio-owner.js

--- plugins/audio-owner.js
+++ plugins/audio-owner.js
@@ -8,19 +8,17 @@ conn.sendFile(m.chat, vn, 'Ownerku.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true , contextInfo: { showAdAttribution: true,
          externalAdReply: { 
-             title: 'Ini nomor ownerku kak klik aja [Nan Sakkarin] https://wa.me/qr/HSXPTGENIOTKJ1',  
+             title: 'Ini nomor ownerku kak klik aja [Nan Sakkarin]',  
              body: 'Jangan lupa sv ya:v', 
              description: '', 
              mediaType: 2, 
            thumbnail: await (await fetch('https://telegra.ph/file/1e2f25d05bcf67d31c73e.jpg')).buffer(), 
-          mediaUrl: `https://youtu.be/35w7z9QFLwY` 
+          mediaUrl: `https://wa.me/qr/HSXPTGENIOTKJ1` 
          } 
       } 
    })
 }
-handler.help = ['owner']
-handler.tags = ['info']
-handler.command = /^(owner)$/i
-handler.fail = null
-handler.exp = 100
+handler.command = handler.help = ['owner']
+handler.tags = ['audio']
+
 export default handler
